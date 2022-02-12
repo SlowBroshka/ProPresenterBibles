e8a9f2e698743e2e7a79429f3048cbfa44d6d122
vladk <vlad.kuz12@gmail.com>
2022-02-12 16:13:01 +0500

    Add NIV parser
    Refs #8

--- src/bible_adapter.py
+++ src/bible_adapter.py
@@ -2,14 +2,24 @@ import os
 
 from src.utils.Utils import Utils
 from src.BibHelp.Parser.VisioBibleParser.VisioBibleParser import VisioBibleParser
+from src.BibHelp.Parser.USXParser.USXBibleParser import USXBibleParser
 from src.dump.DBDumper import SQLiteDBumper
 from src.dump.USXDumper import USXDumper
 from src.usx.simpleRuUSXWriter.SimpleRuUSXWriter import SimpleRuUSXWriter
 
 # For visiobible to usx and sql
-DB_NAME = '../tmp/bible.db3'
-VISIOBIBLE_FOLDER_PATH = r'/home/vladk/Desktop/RU_RST/'
-USX_RES_FOLDER = r'../tmp/usx'
+DB_NAME = '../tmp/niv_bible.db3'
+VISIOBIBLE_FOLDER_PATH = r'/home/vladk/Рабочий стол/NIV_SRS/'
+USX_RES_FOLDER = r'../tmp/niv_usx'
+
+
+def parse_niv_usx_and_dump_to_sql_and_usx(path: str, sql_dump_path: str, usx_dump_path: str):
+    usx_res_path = Utils.prep_path(usx_dump_path)
+
+    bible_parser = USXBibleParser()
+    bible = bible_parser.parse_all(path)
+
+    bible.print_all_books_info()
 
 
 def parse_ru_visiobible_and_dump_to_sql_and_usx(ru_visiobible_path: str,
@@ -49,9 +59,13 @@ def parse_simple_txt_and_dump_to_sql_and_usx():
 
 
 def main():
-    parse_ru_visiobible_and_dump_to_sql_and_usx(ru_visiobible_path=VISIOBIBLE_FOLDER_PATH,
-                                                sql_dump_path=DB_NAME,
-                                                usx_dump_path=USX_RES_FOLDER)
+    # parse_ru_visiobible_and_dump_to_sql_and_usx(ru_visiobible_path=VISIOBIBLE_FOLDER_PATH,
+    #                                             sql_dump_path=DB_NAME,
+    #                                             usx_dump_path=USX_RES_FOLDER)
+
+    parse_niv_usx_and_dump_to_sql_and_usx(path=VISIOBIBLE_FOLDER_PATH,
+                                          sql_dump_path=DB_NAME,
+                                          usx_dump_path=USX_RES_FOLDER)
 
 
 if __name__ == '__main__':
